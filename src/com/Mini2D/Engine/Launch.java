e2c529b6c5d7568bc1d94a700255a2797ca114f3
LINKHA <421698843@qq.com>
2018-09-20 21:32:40 +0800

    #Open mini2D project

---
+++ src/com/Mini2D/Engine/Launch.java
@@ -0,0 +1,10 @@
+package com.Mini2D.Engine;
+
+public class Launch {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Hello Mini2D!!!");
+	}
+
+}
