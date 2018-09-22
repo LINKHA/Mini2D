87c9d923f3e970feb13445d647fecdf3dd896568
DESKTOP-ONT7HKL\计紫铭 <649602986@qq.com>
2018-09-22 21:55:37 +0800

    #Add Paintsizeplace to

--- Source/com/Mini2D/Engine/Screen.java
+++ Source/com/Mini2D/Engine/Screen.java
@@ -6,16 +6,33 @@ import javax.swing.JPanel;
 
 
 public class Screen extends JPanel{
-
+	private int scale=1;
+	private int x,y;
+	private int height,width;
+	private String Image="Assets/t.png";
 	/// V1.0
 	private static final long serialVersionUID = 1L;
+	//private static final Graphics Graphics = null;
 	/**
 	* @brief : This function is called once per frame to render the component
 	*/
 	public synchronized void paintComponent(Graphics g) {
 		super.paintComponent(g);
-
-		g.drawImage(LoadImage.load("Assets/t.png"), 100, 100, 100, 100,null);
-
+		x=200;y=200;height=50;width=50;
+		g.drawImage(LoadImage.load(Image), x, y, width*scale, height*scale,null);
+		x=100;y=100;height=50;width=50;
+		g.drawImage(LoadImage.load(Image), x, y, width*scale, height*scale,null);
+		
+	}
+	public void Scale(int scale) {
+		this.scale=scale;
+	}
+	public void PaintSizePlace(String Image,int x,int y,int width,int height) {
+		this.Image=Image;
+		this.x=x;
+		this.y=y;
+		this.width=width;
+		this.height=height;
+		//paintComponent(Graphics);
 	}
 }
