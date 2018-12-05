2747a68d7a17a5e67a21f8f25349e3e38cf7ae9f
DESKTOP-ONT7HKL\计紫铭 <649602986@qq.com>
2018-12-05 12:48:45 +0800

    #change gravity

--- Source/com/Mini2D/Engine/Gravity.java
+++ Source/com/Mini2D/Engine/Gravity.java
@@ -2,9 +2,11 @@ package com.Mini2D.Engine;
 
 public class Gravity {
 	GameObject gameObject;
-	Vector2 gravityVec;
 	int time;
-	void Down() {
+	Vector2 gravityVec;
+	void Down(GameObject gameObject,int time) {
+		gameObject=this.gameObject;
+		time=this.time;
 		for(int i=0;i<time;i++)
 			gameObject.move(0, (float) (9.8*i));
 	}
